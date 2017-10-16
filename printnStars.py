f12dab8e9193665c68248a4eab73a71dfce4f349
garyyli <garyli@hanovernorwichschools.org>
2017-10-16 14:36:26 -0400

    Updated from Brython Server: 10/16/2017 2:36:26 PM

--- printnStars.py
+++ printnStars.py
@@ -3,7 +3,6 @@
 #printnStars.py
 
 def printnStars(num1):
-    for i in range (0, num1+1):
-        print('*',*num1)
-
-printnStars(3)
+    for i in range (0, num1):
+        print('*'*i)
+printnStars(6)
