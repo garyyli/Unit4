a836f730e03ab78589c968bca8df879504429336
garyyli <garyli@hanovernorwichschools.org>
2017-10-16 14:48:08 -0400

    Updated from Brython Server: 10/16/2017 2:48:07 PM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -1,1 +1,12 @@
+#Gary Li
+#10/16/17
+#colorChangeWindow.py
 
+from ggame import *
+
+def mouseClick(event):
+    Rectangle = RectangleAsset(200,100)
+    Sprite(Rectangle)
+
+App().listenMouseEvent('click',mouseClick)
+App.run()
