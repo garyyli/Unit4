639f40a9bb8adb1756092a4f9d5bf61e3e4c9c50
garyyli <garyli@hanovernorwichschools.org>
2017-10-16 14:54:11 -0400

    Updated from Brython Server: 10/16/2017 2:54:11 PM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -2,11 +2,18 @@
 #10/16/17
 #colorChangeWindow.py
 
+from math import randint
 from ggame import *
 
 def mouseClick(event):
-    Rectangle = RectangleAsset(200,100)
+    red = Color(0xFF0000,1)
+    green = Color(0x00FF00,1)
+    blue = Color(0x0000FF,1)
+    black = Color(0x000000,1)
+    blackOutline = LineStyle(1,black)
+
     Sprite(Rectangle)
 
+mouseClick(event)
 App().listenMouseEvent('click',mouseClick)
 App.run()
