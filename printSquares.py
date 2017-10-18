98d09624d0a684772eaf591a83c6c91d08fd61d3
garyyli <garyli@hanovernorwichschools.org>
2017-10-18 13:54:31 -0400

    Updated from Brython Server: 10/18/2017 1:54:31 PM

--- printSquares.py
+++ printSquares.py
@@ -1,1 +1,11 @@
+#Gary Li
+#10/18/17
+#printSquares
 
+def printSquares(num1,num2):
+    for i in range(1,num1+1):
+        print('+--'*num2+'+')
+        print('|  '*num2+'|')
+    print('+--'*num2+'+')
+    
+printSquares(10,4)
