77610ce77241c91f98893373006269ca0b65a5df
garyyli <garyli@hanovernorwichschools.org>
2017-10-19 14:31:11 -0400

    Updated from Brython Server: 10/19/2017 2:31:13 PM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -10,7 +10,8 @@ COLS = 40
 CELL_SIZE = 20
 
 def moveRight(event):
-    monkey.x += CELL_SIZE
+    if monkey.x<(COLS-1)*CELL_SIZE:
+        monkey.x += CELL_SIZE
 def moveLeft(event):
     monkey.x -= CELL_SIZE
 def moveDown(event):
