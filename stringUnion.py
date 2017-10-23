8984880f99216df537988fd26f3ba9d492fdca10
garyyli <garyli@hanovernorwichschools.org>
2017-10-23 14:51:10 -0400

    Updated from Brython Server: 10/23/2017 2:51:11 PM

--- stringUnion.py
+++ stringUnion.py
@@ -1,3 +1,22 @@
 #Gary Li
 #10/20/17
 #stringUnion.py
+
+def stringUnion(str1,str2):
+    totalStr = ''
+    totalStr += str1
+        
+    totalStr +=str2
+    finalStr = ''
+    for ch in totalStr:
+        if ch in str1:
+            if not ch in finalStr:
+                finalStr += ch
+        if ch in str2:
+            if not ch in finalStr:
+                finalStr+= ch
+            
+            
+    return finalStr
+    
+print(stringUnion('mississippi','pennsyvania'))
