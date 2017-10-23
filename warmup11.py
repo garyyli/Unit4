017f61182ab26ef6eb75c18f65811c0b0da264ee
garyyli <garyli@hanovernorwichschools.org>
2017-10-23 14:30:35 -0400

    Updated from Brython Server: 10/23/2017 2:30:36 PM

--- warmup11.py
+++ warmup11.py
@@ -6,7 +6,8 @@ def isPrime(n):
     for i in range(2,n+1):
         if n%i == 0:
             return False
+        return True
     else:
         return True
 
-print(isPrime(9))
+print(isPrime(20))
