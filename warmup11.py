fcfeda200ab50941b30e4fb825b4025190ad38cc
garyyli <garyli@hanovernorwichschools.org>
2017-10-23 14:29:16 -0400

    Updated from Brython Server: 10/23/2017 2:29:16 PM

--- warmup11.py
+++ warmup11.py
@@ -1,1 +1,12 @@
+#Gary Li
+#10/23/17
+#warmup11.py - determine if a number is prime
 
+def isPrime(n):
+    for i in range(2,n+1):
+        if n%i == 0:
+            return False
+    else:
+        return True
+
+print(isPrime(9))
