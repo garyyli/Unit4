118225501f97404403f7c53460a02d628cc077b3
garyyli <garyli@hanovernorwichschools.org>
2017-10-25 13:38:58 -0400

    Updated from Brython Server: 10/25/2017 1:38:57 PM

--- stringIntersect.py
+++ stringIntersect.py
@@ -1,3 +1,18 @@
 #Gary Li
 #10/25/17
 #stringIntersect.py
+
+def stringIntersect(str1,str2):
+    totalStr = ''
+    totalStr += str1
+    totalStr +=str2
+    finalStr = ''
+    for ch in totalStr:
+        if ch in str1 and ch in str2:
+            if not ch in finalStr:
+                finalStr+= ch
+            
+            
+    return finalStr
+    
+print(stringIntersect('mississippi','pennsylvania'))
