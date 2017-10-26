19b6e8830e62ff656ac1c7be8740846bfc5c491f
garyyli <garyli@hanovernorwichschools.org>
2017-10-26 14:52:08 -0400

    Updated from Brython Server: 10/26/2017 2:52:09 PM

--- multiply.py
+++ multiply.py
@@ -1,1 +1,18 @@
+#Gary Li
+#10/26/2017
+#multiply.py
 
+from random import randint
+
+i=0
+while i<6:
+    def multiply(randint(1,12),randint(1,12)):
+        answer = int(input('What is the answer?: '))
+        if answer == randint(1,12)*randint(1,12):
+            i = i+1
+        else:
+            print('Sorry, that is incorrect.')
+
+print('You are so good at multiplication!')
+        
+    
