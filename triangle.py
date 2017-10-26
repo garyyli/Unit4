ae696bec75ce340ce9287b4a87e9a5f8af476a82
garyyli <garyli@hanovernorwichschools.org>
2017-10-26 14:37:33 -0400

    Updated from Brython Server: 10/26/2017 2:37:34 PM

--- triangle.py
+++ triangle.py
@@ -4,12 +4,12 @@
 
 from math import sqrt
 
-x1 = int(input('Enter x1: '))
-y1 = int(input('Enter y1: '))
-x2 = int(input('Enter x2: '))
-y2 = int(input('Enter y2: '))
-x3 = int(input('Enter x3: '))
-y3 = int(input('Enter y3: '))
+x1 = float(input('Enter x1: '))
+y1 = float(input('Enter y1: '))
+x2 = float(input('Enter x2: '))
+y2 = float(input('Enter y2: '))
+x3 = float(input('Enter x3: '))
+y3 = float(input('Enter y3: '))
 
 
 def distance(a, b, c, d):
@@ -19,5 +19,10 @@ sideA = distance(x1, x2, y1, y2)
 sideB = distance(x2, x3, y2, y3)
 sideC = distance(x3, x1, y3, y1)
 
-def semiperimeter(sideA,sideB,sideC):
+def semiperimeter():
     return 0.5*(sideA+sideB+sideC)
+
+def area():
+    return sqrt(semiperimeter()*(semiperimeter()-sideA)*(semiperimeter()-sideB)*(semiperimeter()-sideC))
+
+print('The area is',area())
