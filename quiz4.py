9ee4225ef96bebe634cf3413554a994fdad2e302
garyyli <garyli@hanovernorwichschools.org>
2017-10-30 14:37:56 -0400

    Updated from Brython Server: 10/30/2017 2:37:59 PM

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,22 @@
+#Gary Li
+#10/30/17
+#quiz4.py
 
+def csia():
+        print('computer science is awesome '*5)
+csia()
+        
+def average(a,b,c):
+    return ((a+b+c)/3)
+print(average(1,2,3))
+
+def lastLetter():
+    for ch in lastLetter():
+        prin
+        
+def same(x,y):
+    if x==y:
+        return True
+    else:
+        return False
+print(same(2*3,7-1))
