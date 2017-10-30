1afcf1d635c34c38d151f78c8b43e0d842b2b8c3
garyyli <garyli@hanovernorwichschools.org>
2017-10-30 14:47:28 -0400

    Updated from Brython Server: 10/30/2017 2:47:30 PM

--- quiz4.py
+++ quiz4.py
@@ -3,16 +3,21 @@
 #quiz4.py
 
 def csia():
-        print('computer science is awesome '*5)
+    for i in range(1,6):
+        print('Computer Science is Awesome')
 csia()
         
 def average(a,b,c):
     return ((a+b+c)/3)
 print(average(1,2,3))
 
-def lastLetter():
+def lastLetter(word):
+    total = 0
     for ch in lastLetter():
-        prin
+        total = total + 1
+    if total == len(lastLetter())
+        print(ch)
+lastLetter('Smedinghoff')
         
 def same(x,y):
     if x==y:
